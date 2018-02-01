de53dc70366f33e911ee40af0c059d59dfdf0617
Chung <chung@lakoo.com>
2018-02-01 19:36:15 +0800

    save

--- contracts/CreatorsPool.sol
+++ contracts/CreatorsPool.sol
@@ -17,78 +17,30 @@
 
 pragma solidity ^0.4.18;
 
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Claimable.sol";
 import "./LikeCoin.sol";
 
-contract CreatorsPool {
+contract CreatorsPool is Claimable {
+    using SafeMath for uint256;
+
     LikeCoin public like = LikeCoin(0x0);
-    address[] public owners;
-    uint8 public threshold = 0;
-    uint public mintTime = 0;
+    uint public mintSlotLength = 0;
+    uint public prevSlot = 0;
     uint256 public mintValue = 0;
 
-    // returns a number which is a power of 2, so we can use uint256 and bitwise operations to simulate a set
-    mapping (address => uint256) ownerIndex;
-
-    // avoid using 0 as fields in proposals are by default initialized to 0
-    uint64 minUsableId = 1;
-    uint64 nextId = 1;
-
-    struct Proposal {
-        uint64 id;
-        address proposer;
-        uint8 confirmNeeded;
-        uint256 confirmedTable;
-    }
-    event ProposalConfirmation(uint64 indexed _id, address _confirmer);
-    event ProposalExecution(uint64 indexed _id, address _executer);
-
-    struct TransferInfo {
-        uint64 id;
-        address to;
-        uint256 value;
-    }
-    event TransferProposal(uint64 indexed _id, address _proposer, address _to, uint256 _value);
-
-    struct SetOwnersInfo {
-        uint64 id;
-        uint8 newThreshold;
-        address[] newOwners;
-    }
-    event SetOwnersProposal(uint64 indexed _id, address _proposer, address[] _newOwners, uint8 _newThreshold);
-
-    mapping (uint64 => Proposal) proposals;
-    mapping (uint64 => TransferInfo) transferInfo;
-    mapping (uint64 => SetOwnersInfo) setOwnersInfo;
-
-    function CreatorsPool(address _likeAddr, address[] _owners, uint8 _threshold, uint _mintTime, uint256 _mintValue) public {
-        require(_owners.length < 256);
-        require(_owners.length > 0);
-        require(_threshold > 0);
-        require(_owners.length >= _threshold);
+    function CreatorsPool(address _likeAddr, uint _mintSlotLength, uint256 _mintValue) public {
+        require(_mintValue > 0);
+        require(_mintSlotLength > 0);
         like = LikeCoin(_likeAddr);
-        for (uint8 i = 0; i < _owners.length; ++i) {
-            owners.push(_owners[i]);
-            require(ownerIndex[_owners[i]] == 0);
-            ownerIndex[_owners[i]] = uint256(1) << i;
-        }
-        threshold = _threshold;
-        mintTime = _mintTime;
         mintValue = _mintValue;
+        prevSlot = now / _mintSlotLength - 1;
     }
 
-    function ownersCount() public constant returns (uint) {
-        return owners.length;
-    }
-
-    function _nextId() internal returns (uint64 id) {
-        id = nextId;
-        nextId += 1;
-        return id;
-    }
-
-    function mint() public {
-        require(now >= mintTime);
-        like.mintForCreatorsPool(mintValue);
+    function mint(uint256 _value) public {
+        uint slotNow = now / mintSlotLength;
+        uint256 mintAllowance = slotNow.sub(prevSlot).mul(mintValue);
+        like.mintForCreatorsPool(mintAllowance);
     }
 
     function proposeTransfer(address _to, uint256 _value) public {
